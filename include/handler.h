3e817f975e25cb6bd424c090a4b53aff8ba5c3f5
DreamCWeaver <115027823+OOriakhi@users.noreply.github.com>
2025-12-13 10:15:18 -0500

    handler updates
    handler up to date

--- include/handler.h
+++ include/handler.h
@@ -10,4 +10,14 @@
 // Sprint 2: full HTTP request parsing + file serving
 void handle_connection_stub(int client_file_descriptor);
 
+static int send_all(int fd, const char *buf, size_t len);
+
+static const char *get_mime_type(const char* path);
+
+static void send_http_response( int fd, int status, const char *status_text, const char *content_type, const char *body);
+
+static void serve_file(int fd, const char *path);
+
+
+
 #endif
